be9e2b66980bae6e30c1643fc177d61bcbca9b04
Mr_Starkk <yashshr09@gmail.com>
2021-08-03 23:06:24 +0530

    initial look

---
+++ tic_tac_toe.js
@@ -0,0 +1,10 @@
+const gameBoard = (function(){
+    let gameBoard_array = ['X','O','X','O','X','X','X','O','O'];
+})();
+
+const player = function(name,marker){
+    return ({name,marker})
+}
+const gameFlow = (function(){
+
+})();
