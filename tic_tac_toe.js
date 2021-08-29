acc8be04ae6d2d6c7391c645433f1677b0e49da6
Mr_Starkk <yashshr09@gmail.com>
2021-08-30 00:00:56 +0530

    displayController

--- tic_tac_toe.js
+++ tic_tac_toe.js
@@ -1,10 +1,29 @@
 const gameBoard = (function(){
     let gameBoard_array = ['X','O','X','O','X','X','X','O','O'];
+    return {
+        gameBoard_array,
+    }
 })();
 
 const player = function(name,marker){
-    return ({name,marker})
+    return {
+        name,
+        marker
+    }
 }
-const gameFlow = (function(){
+const gameFlow = (function(array){
+    const displayController = () => {
+        console.log('i was here');
+        for(i=0; i<array.length; i++){
+            const sq = document.getElementById(`sq${i}`);
+            sq.innerText = array[i];
+        }
+    }
+    return {
+        displayController,
+    }
 
-})();
+})(gameBoard.gameBoard_array);
+
+
+gameFlow.displayController()
